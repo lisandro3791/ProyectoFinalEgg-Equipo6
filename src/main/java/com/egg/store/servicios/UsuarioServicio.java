36ae89f5a1847ed588411011f7625aa4007ca3d9
lisandro3791 <lisandro3791@gmail.com>
2021-06-22 20:03:12 -0300

    Create UsuarioServicio.java

---
+++ src/main/java/com/egg/store/servicios/UsuarioServicio.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egg.store.servicios;
+
+import com.egg.store.entidades.Usuario;
+import com.egg.store.repositorios.UsuarioRepositorio;
+import java.util.Date;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Mecha
+ */
+@Service
+public class UsuarioServicio {
+    
+    @Autowired
+       private UsuarioRepositorio usuarioRepositorio;
+ 
+     @ Transactional
+    
+    public void crear(String nombre, String apellido ,  Date nacimiento, long dni){
+        
+        Usuario usuario = new Usuario ();
+        usuario.setNombre(nombre);
+        usuario.setApellido(apellido);
+        usuario.setNacimiento(nacimiento);
+        usuario.setDni(dni);
+        
+        usuarioRepositorio.save(usuario);
+                       
+    }
+    
+     @ Transactional
+    public List <Usuario> buscarTodos() {
+             List<Usuario> usuarios = usuarioRepositorio.findAll();
+        return usuarios;
+        
+        
+        
+    }
+    
+}
