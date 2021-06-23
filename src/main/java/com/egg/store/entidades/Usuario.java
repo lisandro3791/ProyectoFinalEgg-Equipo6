88fda1a4a7dd7d330f5cd6611a72a5b11629f954
lisandro3791 <lisandro3791@gmail.com>
2021-06-23 19:51:58 -0300

    Update Usuario.java

--- src/main/java/com/egg/store/entidades/Usuario.java
+++ src/main/java/com/egg/store/entidades/Usuario.java
@@ -12,8 +12,8 @@ import org.hibernate.annotations.GenericGenerator;
 
 public class Usuario implements Serializable {
     @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator( name= "uuid" , strategy = " uuid2") 
+    @GeneratedValue(generator = "uuid3")
+    @GenericGenerator( name= "uuid3" , strategy = " uuid5") 
     private String id;
     
     private String nombre;
