560e22fc49dcdcb27ec838b6d0542a4d12783c4c
Lucas Garcia <leonardo.lucasgarcia@gmail.com>
2021-06-22 22:07:51 -0300

    procesando error

--- src/main/java/com/egg/store/controladores/UsuarioControlador.java
+++ src/main/java/com/egg/store/controladores/UsuarioControlador.java
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.egg.store.controladores;
 
 import com.egg.store.entidades.Usuario;
 import com.egg.store.servicios.UsuarioServicio;
 import java.util.Date;
 import java.util.List;
-=======
-package com.egg.store.controladores;
-
-import java.util.Date;
->>>>>>> 3200c76 (Creando el controlador de usuario para el registro)
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
@@ -25,21 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-<<<<<<< HEAD
-/**
- *
- * @author Mecha
- */
 
 @Controller
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 public class UsuarioControlador {
-    
-    
+
     @Autowired
     private UsuarioServicio usuarioServicio;
-    
-    @GetMapping("/ver-todos") 
+
+       @GetMapping("/ver-todos") 
     public ModelAndView buscarTodos(){
         ModelAndView mav = new ModelAndView("usuarios-lista");
         List<Usuario>usuarios = usuarioServicio.buscarTodos();
@@ -48,32 +32,6 @@ public class UsuarioControlador {
         
     }
     
-    @GetMapping("/crear")
-    public ModelAndView mostrarFormulario(){
-        return new ModelAndView("usuario-formulario");
-    }
-    
-    
-    @PostMapping("/guardar")
-    public RedirectView guardar( @RequestParam String nombre,@RequestParam String apellido, @RequestParam String contraseña, @RequestParam String mail,
-            @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd" ) Date nacimiento, @RequestParam long dni){
-    
-            usuarioServicio.crear(nombre, apellido,contraseña,mail, nacimiento, dni);
-       
-        return new RedirectView("/usuarios/ver-todos") ;
-    }
-    
-    
-}
-
-=======
-@Controller
-@RequestMapping("/usuario")
-public class UsuarioControlador {
-
-    @Autowired
-    private UsuarioServicio usuarioServicio;
-
     @GetMapping("/crear")
     public ModelAndView formularioRegistro(){
     return new ModelAndView("/registrarse");
@@ -84,8 +42,7 @@ public class UsuarioControlador {
             @RequestParam String nombre, @RequestParam String apellido,
             @RequestParam String password, @RequestParam long dni,
             @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date nacimiento, @RequestParam String email) {
-        usuarioServicio.crear(nombre, apellido, password, dni, nacimiento, email);
+        usuarioServicio.crear(nombre, apellido, password, email, nacimiento, dni);
         return new RedirectView("/perfil");
     }
 }
->>>>>>> 3200c76 (Creando el controlador de usuario para el registro)
