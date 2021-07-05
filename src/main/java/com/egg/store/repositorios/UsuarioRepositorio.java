66b5e35b3dcdb79de33a84cd2dd8ef157dd238d0
lisandro3791 <lisandro3791@gmail.com>
2021-07-05 11:40:38 -0300

    Update UsuarioRepositorio.java

--- src/main/java/com/egg/store/repositorios/UsuarioRepositorio.java
+++ src/main/java/com/egg/store/repositorios/UsuarioRepositorio.java
@@ -6,7 +6,12 @@
 package com.egg.store.repositorios;
 
 import com.egg.store.entidades.Usuario;
+import java.util.Date;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,6 +20,20 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario,String> {
+   
+    
+    //Modifica un Usuario en los parametros que se pasan cuando coincide el campo de DNI
+    @Modifying
+    @Query("UPDATE Usuario u SET u.nombre = :nombre, u.apellido = :apellido, u.fechaNacimiento = :fechaNacimiento WHERE u.dni = :dni")
+    void modificar (@Param ("dni") Long dni, @Param("nombre") String nombre, @Param ("apellido") String apellido, @Param("fechaNacimiento") Date fechaNacimiento);
+    
+    //lista todos los usuarios por nombre
+    @Query("SELECT u FROM Usuario u WHERE u.nombre= :n")
+    List<Usuario> buscarPorNombre(@Param ("n") String nombre);
+    
+    
+    @Query("SELECT u FROM Usuario u WHERE u.usuario = :username")
+    Usuario buscarPorNombreDeUsuaruio ( @Param("username") String username);
     
     
 }
