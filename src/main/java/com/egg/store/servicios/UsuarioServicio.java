eeefaf1f53e5bdf53d0f71204df14401d1e5fcea
lisandro3791 <lisandro3791@gmail.com>
2021-07-05 11:40:31 -0300

    Update UsuarioServicio.java

--- src/main/java/com/egg/store/servicios/UsuarioServicio.java
+++ src/main/java/com/egg/store/servicios/UsuarioServicio.java
@@ -7,9 +7,15 @@ package com.egg.store.servicios;
 
 import com.egg.store.entidades.Usuario;
 import com.egg.store.repositorios.UsuarioRepositorio;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,19 +24,23 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Mecha
  */
 @Service
-public class UsuarioServicio {
+public class UsuarioServicio implements UserDetailsService{
     
     @Autowired
        private UsuarioRepositorio usuarioRepositorio;
+    
+    @Autowired
+    private BCryptPasswordEncoder encoder;
  
      @ Transactional
-    
+    //Recibe tambien el parametro username, hace falta agregarlo String username
     public void crear(String nombre, String apellido ,String contrasena,String mail,  Date nacimiento, long dni){
         
         Usuario usuario = new Usuario ();
         usuario.setNombre(nombre);
         usuario.setApellido(apellido);
-        usuario.setContrasena(contrasena);
+     //   usuario.serUsername(username);
+        usuario.setContrasena(encoder.encode(contrasena));
         usuario.setMail(mail);
         usuario.setNacimiento(nacimiento);
         usuario.setDni(dni);
@@ -43,9 +53,31 @@ public class UsuarioServicio {
     public List <Usuario> buscarTodos() {
              List<Usuario> usuarios = usuarioRepositorio.findAll();
         return usuarios;
-        
-        
-        
+
     }
     
+        @Transactional(readOnly =true)
+        public List<Usuario> buscarPorNombre (String nombre){
+            return usuarioRepositorio.buscarPorNombre(nombre);
+        }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+            Usuario usuario = usuarioRepositorio.buscarPorNombreDeUsuaruio(username);
+            if (usuario == null){
+             //   throws new UsernameNotFoundException ("no se encontro ningun usuario con username" + username);
+            }
+
+           User user = new User(usuario.getMail(), usuario.getContrasena(), Collections.emptyList());
+            
+                   
+            
+        return null;
+        }
+
+        
 }
+    
+    
+
